07b9dcbebe6f85279b886dca4e74817fc52a5fa1
Andy Wright <atwright147@gmail.com>
2019-08-01 22:31:19 +0100

    add a date picker page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { DatePickerPageComponent } from './components/date-picker-page/date-picker-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContactsListComponent,
+  },
+  {
+    path: 'date-picker',
+    component: DatePickerPageComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 import { ContactsTableComponent } from './components/contacts-table/contacts-table.component';
 import { SearchResultsTableComponent } from './components/search-results-table/search-results-table.component';
+import { DatePickerPageComponent } from './components/date-picker-page/date-picker-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
     ContactsTableComponent,
-    SearchResultsTableComponent
+    SearchResultsTableComponent,
+    DatePickerPageComponent,
   ],
   imports: [
     BrowserModule,
