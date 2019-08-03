f9264746f5305629b480903ba8a0a56803f87732
Andy Wright <atwright147@gmail.com>
2019-08-03 16:54:25 +0100

    begin implementing a date picker service

---
+++ src/app/services/date-picker.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatePickerService {
+  private _month: number;
+  private _year: number;
+
+  constructor() { }
+
+  get month() {
+    return this._month;
+  }
+
+  set month(month: number) {
+    this._month = month;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  set year(year: number) {
+    this._year = year;
+  }
+
+  get firstDayOfSelectedMonth() {
+    return (new Date(this.year, this.month)).getDay();
+  }
+
+  get numDaysInSelectedMonth() {
+    return 32 - new Date(this.year, this.month, 32).getDate();
+  }
+
+  get numCellsToRender() {
+    let numCells = 0;
+    numCells += this.firstDayOfSelectedMonth;
+    numCells += this.numDaysInSelectedMonth;
+    if (numCells % 7 !== 0) {
+      while (numCells % 7 !== 0) {
+        numCells += 1;
+      }
+    }
+    return numCells;
+  }
+}
