12ae0e60082fb6964863aa3b71029e54a792588f
Andy Wright <atwright147@gmail.com>
2019-08-04 16:18:00 +0100

    add input restriction directive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { FormPageComponent } from './components/form-page/form-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContactsListComponent,
+  },
+  {
+    path: 'form',
+    component: FormPageComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { AppComponent } from './app.component';
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 import { ContactsTableComponent } from './components/contacts-table/contacts-table.component';
 import { SearchResultsTableComponent } from './components/search-results-table/search-results-table.component';
+import { FormPageComponent } from './components/form-page/form-page.component';
+import { FormExampleComponent } from './components/form-example/form-example.component';
+import { NumberSentryDirective } from './directives/number-sentry.directive';
+import { InputRestrictionDirective } from './directives/input-restriction.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
     ContactsTableComponent,
-    SearchResultsTableComponent
+    SearchResultsTableComponent,
+    FormPageComponent,
+    FormExampleComponent,
+    NumberSentryDirective,
+    InputRestrictionDirective,
   ],
   imports: [
     BrowserModule,
